3d4ac0f86d0cf64983d17b0e795569053ca118c5
duckyprogramming2022 <gueney.timur@outlook.de>
2022-09-03 08:04:57 +0200

    added button selector and variables to check button selections

---
+++ js.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelectorAll('button');
+
+const operands = ['1','2','3','4','5','6','7','8','9','.']
+const operators = ['x','/','+','-','=']
+const special = ['Delete','Clear']
+
+
+
+
+
+
+
