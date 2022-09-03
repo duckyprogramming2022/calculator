851f72fa79ddac30cd78d1af7cceb6e36db18813
duckyprogramming2022 <gueney.timur@outlook.de>
2022-09-03 08:05:43 +0200

    added functions

--- js.js
+++ js.js
@@ -5,7 +5,40 @@ const operators = ['x','/','+','-','=']
 const special = ['Delete','Clear']
 
 
+function operations(operation, num1, num2) {
+    switch (operation) {
+        case 'add':
+            return num1 + num2;
+        case 'sub':
+            return num1 - num2;
+        case 'division':
+            return num1/num2;
+        case 'mul':
+            return num1*num2;
+        default:
+            console.log('whooops something went wrong')
+    };
+};
 
+function checkOperands(num_part) {
+    return operands.includes(num_part);
+}
+
+function checkDotIncluded(num) {
+    return num.includes(".");
+}
+
+function checkNumPartPushable(num, num_part) {
+    if (checkOperands(num_part)) {
+        if (!checkDotIncluded(num)) {
+            return true;
+        } else if (checkDotIncluded(num) && num_part !== ".") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
 
 
 
